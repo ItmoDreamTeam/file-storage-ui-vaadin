7e26fb9bd99a5a4634165c636bccfb024ebb35a0
Kirill <smirnowku@gmail.com>
2017-11-04 11:42:47 +0300

    sort file by upload date

--- src/main/java/org/fsgroup/filestorage/client/web/vaadin/model/User.java
+++ src/main/java/org/fsgroup/filestorage/client/web/vaadin/model/User.java
@@ -2,10 +2,17 @@ package org.fsgroup.filestorage.client.web.vaadin.model;
 
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+
 public class User extends BaseEntity {
 
     private String username;
     private List<UploadedFile> files;
+
+    public void setFiles(List<UploadedFile> files) {
+        this.files = files;
+        this.files.sort(comparing(UploadedFile::getCreated).reversed());
+    }
 
     public String getUsername() {
         return username;
